41d9e7b91dd893d082701382615c0ba0fbf21679
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-11-04 08:28:57 +0100

    Add HomeView and update URL routing to include home path

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
-
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet, ProposalUtilityViewSet
+from .views import HomeView, ProjectViewSet, ProposalUtilityViewSet
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='project')
 router.register(r'proposals', ProposalUtilityViewSet, basename='proposal')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('', include(router.urls)),
+]
