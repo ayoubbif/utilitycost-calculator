750928260fa75b20f45cb5c5fed5bba7aa3f3493
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-11-06 21:39:33 +0100

    Refactor views by removing deprecated code and organizing imports into __init__.py

---
+++ app/views/__init__.py
@@ -0,0 +1,11 @@
+from .home_view import HomeView
+from .project_viewset import ProjectViewSet
+from .proposal_utility_viewset import ProposalUtilityViewSet
+from .project_webhook_view import ProjectWebhookViewSet
+
+__all__ = [
+    'HomeView',
+    'ProjectViewSet',
+    'ProposalUtilityViewSet',
+    'ProjectWebhookViewSet'
+    ]
